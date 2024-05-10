dfd2881117ba4c104c20c85f7499a47cd25236a0
PrafullBendale <bendale1prafull@gmail.com>
2024-05-10 11:00:35 +0530

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string CardOwnerName { get; set; } = "";
+
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; } = "";
+
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+
+        [Column(TypeName = "varchar(3)")]
+        public string SecurityCode { get; set; } = "";
+
+    }
+}
